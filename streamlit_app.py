216dab8096ee791f37f416047607b47432169ad1
Mehmet Emin BALOĞLU <baloglu321@gmail.com>
2024-11-24 20:20:45 +0300

    “second”

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,31 @@
 import streamlit as st
+import numpy as np
+from model_utils import *
+import os
+from PIL import Image
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+model = YOLO_MODEL("./yolo.pt")
+ocr = PaddleOCR(lang='tr')
+
+img_file_buffer = st.file_uploader('Upload a image')
+if img_file_buffer is not None:
+    
+    file_name = img_file_buffer.name
+    
+    # Uzantıyı al (örneğin: '.jpg', '.png')
+    file_extension = os.path.splitext(file_name)[1].lower()
+    
+    if file_extension in [".jpg",".JPG",".png",".PNG",".bmp",".BMP",".jpeg",".JPEG"]:
+        #img=cv2.imread(img_file_buffer)
+        img = Image.open(img_file_buffer)
+        img_array = np.array(img)
+        with st.spinner('Wait for it...'):
+            det_img=det(img_array,ocr,model) 
+        st.success("Done!")
+        st.image(det_img, caption="Plate results")
+    else:
+        st.error('Unsupported file extension. Please upload a file with one of the following extensions: .jpg, .JPG, .png, .PNG, .bmp, .BMP ', icon="🚨")
+
+
+else:
+    st.error('This file could not be read', icon="🚨")      
