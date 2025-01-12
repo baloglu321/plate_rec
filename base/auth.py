bb54ec440369741b2cfb844dd4b4a92416c70ecf
Mehmet Emin BALOĞLU <baloglu321@gmail.com>
2025-01-12 16:10:01 +0300

    second commit

---
+++ base/auth.py
@@ -0,0 +1,17 @@
+import yaml
+from yaml.loader import SafeLoader
+import streamlit_authenticator as stauth
+
+with open('./base/auth_config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+# Pre-hashing all plain text passwords once
+# stauth.Hasher.hash_passwords(config['credentials'])
+def auth():
+    authenticator = stauth.Authenticate(
+        config['credentials'],
+        config['cookie']['name'],
+        config['cookie']['key'],
+        config['cookie']['expiry_days']
+    )
+    return authenticator
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,15 @@
 import streamlit as st
+import streamlit_authenticator as stauth
 from model_utils import *
+from base.auth import auth
 
 if 'processor_1' not in st.session_state:
     st.session_state.processor_1 = VideoProcessor.get_instance("processor_1", "./video.mp4", fps=20.0)
 
 if 'processor_2' not in st.session_state:
     st.session_state.processor_2 = VideoProcessor.get_instance("processor_2", "./video-2.mp4", fps=15.0)
+
+
 
 def main():
     st.sidebar.title("Yayınlar")
@@ -32,15 +36,35 @@ def main():
                 placeholder.image(frame, caption="Video Akışı")
             time.sleep(0.033)
 
+authenticator=auth()
 
+def log_out():
+    authenticator.logout()
 
-pages = [
-        st.Page(main,title="Yayınlar"),
-        st.Page("tabs/plates.py", title="Plaka listesi"),
-        st.Page("tabs/saved_videos.py", title="Kayıtlı videolar")
-    ]
+try:
+    authenticator.login()
+except Exception as e:
+    st.error(e)
 
+if st.session_state['authentication_status']:
+    
+    st.sidebar.title(f'Welcome *{st.session_state["name"]}*')
 
+    
+    pages = [
+            st.Page(main,title="Yayınlar"),            
+            st.Page("tabs/plates.py", title="Plaka listesi"),
+            st.Page("tabs/saved_videos.py", title="Kayıtlı videolar"),
+            st.Page(log_out,title="Çıkış Yap")
+            
+        ]
+    
+    pg = st.navigation(pages)
+    pg.run()
+    
 
-pg = st.navigation(pages)
-pg.run()
+elif st.session_state['authentication_status'] is False:
+    st.error('Username/password is incorrect')
+elif st.session_state['authentication_status'] is None:
+    st.warning('Please enter your username and password')
+
